8a146f1ae778a3eb68bd2494398f8660028127a6
Mikk Soone <mikksoone@gmail.com>
2012-01-28 17:37:06 +0200

    Changed tabs to spaces

--- src/MinusTechnique.cpp
+++ src/MinusTechnique.cpp
@@ -23,9 +23,9 @@
 // #define PRINT_DEBUG
 
 
-#define SORT 		   // Could be undef if one want's to compare time differences
-#define MIN_ITEM 0 	// If lowest item is 1, lower every item by 1
-#define INT int 	   // Maybe a double is needed?
+#define SORT          // Could be undef if one want's to compare time differences
+#define MIN_ITEM 0    // If lowest item is 1, lower every item by 1
+#define INT int       // Maybe a double is needed?
 
 // #define HARDCODED_DATA // If we don't want to specify input/output files 
 
@@ -58,12 +58,12 @@ typedef struct {
 /* Helper function for debugging INT arrays 
    TODO: change %d to %f if needed */
 void print_int_arr(INT *arr, INT len, const char *name){
-	INT i;
-	//return;
-	for( i=0 ; i<len ; i++){
-		printf("%d ", arr[i]);
-	}
-	printf ("==%s==========\n", name);
+   INT i;
+   //return;
+   for( i=0 ; i<len ; i++){
+      printf("%d ", arr[i]);
+   }
+   printf ("==%s==========\n", name);
 }
 
 /* allocate memory with error routine (Takeaki Uno)*/
@@ -124,7 +124,7 @@ INT FILE_read_int (FILE *fp){
 /* load a transaction from the input file to memory (Takeaki Uno style)*/
 void TRSACT_file_load (TRSACT *T, const char *fname)
 {
-#ifdef DEBUG_TIMER	
+#ifdef DEBUG_TIMER   
    TIMER("load file");
 #endif
    INT item, i, j;
@@ -136,54 +136,54 @@ void TRSACT_file_load (TRSACT *T, const char *fname)
    if ( (FILE_err&4) != 0){ printf ("file structure error 1\n"); exit (1); }
    nCol = FILE_read_int (fp);
    if ( (FILE_err&4) != 0){ printf ("file structure error 2\n"); exit (1); }
-	
+   
    T->buf = (INT *)alloc_memory ( sizeof(INT) * (nRow*nCol) );
-	
+   
    j = 0; //row id
-	do 
+   do 
    {
-	   i = 0; //col id
+      i = 0; //col id
       do 
       {
-		   item = FILE_read_int (fp);
+         item = FILE_read_int (fp);
          if ( (FILE_err&4) == 0)
          {  // got an item from the file before reaching to line-end
-				T->buf[j*nCol+i] = item-MIN_ITEM; //to get the elements started from 0
-				i++;
+            T->buf[j*nCol+i] = item-MIN_ITEM; //to get the elements started from 0
+            i++;
          }
       } while ((FILE_err&3)==0);
-		j++;
+      j++;
   } while ( (FILE_err&2)==0);
 
-	fclose(fp);
+   fclose(fp);
 }
 
 /* Prints the reordered table to a file */
 void TRSACT_output_result(TRSACT * T, const char *fname)
 {
-#ifdef DEBUG_TIMER	
-	TIMER("output file");
+#ifdef DEBUG_TIMER   
+   TIMER("output file");
 #endif
    INT i, j;
    FILE *fp = fopen (fname,"w");
 
    if ( !fp ){ printf ("file open error\n"); exit (1); }
 
-	for( i=0 ; i<nCol ; i++ )
-	{
-		for( j=0 ; j<nRow ; j++) 
-		{
-			fprintf( fp, "%d ", T->buf[ T->seq[j]*nCol+i ] + MIN_ITEM );
-		}
-		fprintf(fp, "\n");
-	}
-	fclose(fp);
+   for( i=0 ; i<nCol ; i++ )
+   {
+      for( j=0 ; j<nRow ; j++) 
+      {
+         fprintf( fp, "%d ", T->buf[ T->seq[j]*nCol+i ] + MIN_ITEM );
+      }
+      fprintf(fp, "\n");
+   }
+   fclose(fp);
 }
 
 /* Frees the transaction and global data */
 void TRSACT_free( TRSACT * T )
 {
-#ifdef DEBUG_TIMER	
+#ifdef DEBUG_TIMER   
    TIMER("free");
 #endif
    free( T->buf );
@@ -200,7 +200,7 @@ void TRSACT_free( TRSACT * T )
 /* Alloc the neccessary memory for transactions and do the initial calculations */
 void TRSACT_init( TRSACT * T )
 {
-#ifdef DEBUG_TIMER	
+#ifdef DEBUG_TIMER   
    TIMER("init");
 #endif
    INT i,j;
@@ -213,29 +213,29 @@ void TRSACT_init( TRSACT * T )
 
    T->frq_buf = (INT*) alloc_memory ( sizeof(INT) * nCol*nRow );
    T->frq = (INT**) alloc_memory( sizeof(INT) * nCol );
-	
+   
    size_t cnt = 0;
-	for( i=0; i<nCol ; i++ )
-	{
+   for( i=0; i<nCol ; i++ )
+   {
       // We just keep track and don't allocate for every col: (INT*)alloc_memory( sizeof(INT) * T->col_max[i] );
       T->frq[i] = &T->frq_buf[cnt]; 
       cnt += T->col_max[i];
-	}
+   }
    // Fill the frequency table
-	for( j=0 ; j<nRow ; j++)
-		for( i=0 ; i<nCol ; i++ )
-			++T->frq[i][ T->buf[j*nCol + i] ];
+   for( j=0 ; j<nRow ; j++)
+      for( i=0 ; i<nCol ; i++ )
+         ++T->frq[i][ T->buf[j*nCol + i] ];
 
 
-	T->conform		= (INT*) alloc_memory( sizeof(INT) * nRow );
-	T->seq			= (INT*) alloc_memory( sizeof(INT) * nRow );
-	T->seq_count   = 0;
+   T->conform      = (INT*) alloc_memory( sizeof(INT) * nRow );
+   T->seq         = (INT*) alloc_memory( sizeof(INT) * nRow );
+   T->seq_count   = 0;
 
    // Fill the conform table
-	g_conform = (INT*) alloc_memory( sizeof(INT) * nRow );
-	for( j=0 ; j<nRow ; j++)
-		for( i=0 ; i<nCol ; i++ )
-			g_conform[j] += T->frq[i][ T->buf[j*nCol + i] ];
+   g_conform = (INT*) alloc_memory( sizeof(INT) * nRow );
+   for( j=0 ; j<nRow ; j++)
+      for( i=0 ; i<nCol ; i++ )
+         g_conform[j] += T->frq[i][ T->buf[j*nCol + i] ];
 
    
    // We add number from 0 to nRow to the rows_left vector
@@ -251,7 +251,7 @@ void TRSACT_init( TRSACT * T )
 /* Here we transform the file buffer from vertical to horizontal */
 void TRSACT_switch(TRSACT * T)
 {
-#ifdef DEBUG_TIMER	
+#ifdef DEBUG_TIMER   
    TIMER("switch");
 #endif
    printf("Switching..\n");
@@ -259,24 +259,24 @@ void TRSACT_switch(TRSACT * T)
    // Fill the tmp container with values from ordered buf 
    INT * tmp = (INT*) alloc_memory( sizeof(INT) * nRow*nCol );
    for( i=0 ; i<nCol ; i++ )
-      for( j=0 ; j<nRow ; j++)	
+      for( j=0 ; j<nRow ; j++)   
          tmp[cnt++] = T->buf[ T->seq[j]*nCol +i ]; 
-	
+   
 #ifdef PRINT_DEBUG
    for( j=0; j<nRow ; j++ )
       print_int_arr(&T->buf[T->seq[j]*nCol], nCol, "buf ordered");
 #endif
-   // Free the currently used transaction container	
+   // Free the currently used transaction container   
    TRSACT_free(T);
 
-	T->buf = tmp;
-	INT k = nRow;
-	nRow = nCol;
-	nCol = k;
+   T->buf = tmp;
+   INT k = nRow;
+   nRow = nCol;
+   nCol = k;
 
 #ifdef PRINT_DEBUG
-	for( j=0 ; j<nRow ; j++ )
-		print_int_arr(&T->buf[j*nCol], nCol, "buf switched");
+   for( j=0 ; j<nRow ; j++ )
+      print_int_arr(&T->buf[j*nCol], nCol, "buf switched");
 #endif
 
    // Init again..
@@ -311,7 +311,7 @@ static int loops_after_sort = 0;
 */
 static inline void sort(TRSACT * T)
 {
-#ifdef DEBUG_TIMER	
+#ifdef DEBUG_TIMER   
    TIMER("sort");
 #endif 
    // We measure the time it takes to calculate all coforms and sort the rows
@@ -332,8 +332,8 @@ static inline void sort(TRSACT * T)
 /* Routine for finding the row with minimum conform */
 static inline bool find_min(TRSACT * T)
 {
-#ifdef DEBUG_TIMER	
-	TIMER("find_min");
+#ifdef DEBUG_TIMER   
+   TIMER("find_min");
 #endif
    static TIMER_TYPE start_time = 0;
    start_time = get_time();
@@ -372,7 +372,7 @@ static inline bool find_min(TRSACT * T)
    printf( "row=%d conform=%d\n", min_row, T->conform[min_row] );
    print_int_arr(&T->buf[min_row*nCol], nCol, "eliminated row");
 #endif
-   // Remove the min row from the vector of rows left		
+   // Remove the min row from the vector of rows left      
    T->rows_left.erase( it_remove );
 
    if ( T->rows_left.empty() )
@@ -383,9 +383,9 @@ static inline bool find_min(TRSACT * T)
    // Update the frequency table
    for( i=0 ; i<nCol ; i++ )
       --T->frq[i][ T->buf[min_row*nCol + i] ];
-	
+   
    g_find_time += get_time() - start_time;
-	return true;
+   return true;
 }
 
 /* Function that calculates when to sort*/
@@ -393,116 +393,116 @@ static inline void calculate_sort_threshold()
 {
    // Somewhat awkward way to calculate how many items we should throw out before sorting again
    // Currently we calculate it based on sorting and finding times
-	static double ratio;
+   static double ratio;
    ratio = double(g_sort_time)/double(g_find_time);
 
-	if( ratio < 0.5)
-		SORT_THRESHOLD /= 1.5;
-	else if (ratio < 0.7 )
-		SORT_THRESHOLD /= 1.3;
-	else if ( ratio > 10 )
-		SORT_THRESHOLD *= 3;
-	else if( ratio > 5)
-		SORT_THRESHOLD *= 2;
-	else if ( ratio > 3 )
-		SORT_THRESHOLD *= 1.2;
-	if( SORT_THRESHOLD == 0 )
-		SORT_THRESHOLD = 1;
+   if( ratio < 0.5)
+      SORT_THRESHOLD /= 1.5;
+   else if (ratio < 0.7 )
+      SORT_THRESHOLD /= 1.3;
+   else if ( ratio > 10 )
+      SORT_THRESHOLD *= 3;
+   else if( ratio > 5)
+      SORT_THRESHOLD *= 2;
+   else if ( ratio > 3 )
+      SORT_THRESHOLD *= 1.2;
+   if( SORT_THRESHOLD == 0 )
+      SORT_THRESHOLD = 1;
 
    // Can't do like this (dynamically calcualte), because one abnormality can make the threshold so big,
    // that the app will actually never sort
    // SORT_THRESHOLD = int( double(SORT_THRESHOLD) * double(g_sort_time)/double(g_find_time) );
-	
+   
 }
 /* Iteration to order the table */
 void minus(TRSACT * T)
 {
-#ifdef DEBUG_TIMER	
-	TIMER("minus");
+#ifdef DEBUG_TIMER   
+   TIMER("minus");
 #endif
-	loops_after_sort = 0;
-	SORT_THRESHOLD = 3;
+   loops_after_sort = 0;
+   SORT_THRESHOLD = 3;
 
-	do
-	{
-#ifdef PRINT_DEBUG	
-	for( INT i=0 ; i<nCol ; i++ )
-		print_int_arr(T->frq[i], T->col_max[i], "frq");
+   do
+   {
+#ifdef PRINT_DEBUG   
+   for( INT i=0 ; i<nCol ; i++ )
+      print_int_arr(T->frq[i], T->col_max[i], "frq");
 #endif
 
-		if ( ! find_min( T) ) 
-			return; // The end
-#ifdef SORT		
-		if ( loops_after_sort % SORT_THRESHOLD  == 0 )
-		{
-			sort(T);
+      if ( ! find_min( T) ) 
+         return; // The end
+#ifdef SORT      
+      if ( loops_after_sort % SORT_THRESHOLD  == 0 )
+      {
+         sort(T);
 #ifdef PRINT_DEBUG
-			printf("sort/find: %4.2f, threshold=%d\n", double(g_sort_time)/double(g_find_time), SORT_THRESHOLD);
+         printf("sort/find: %4.2f, threshold=%d\n", double(g_sort_time)/double(g_find_time), SORT_THRESHOLD);
 #endif
-			calculate_sort_threshold();
-			loops_after_sort=0;
-			g_find_time = 0;
-		}
+         calculate_sort_threshold();
+         loops_after_sort=0;
+         g_find_time = 0;
+      }
 #endif
       // We always find new conforms, and don't update the old ones using -- etc
-		memset( T->conform, 0, sizeof(INT) * nRow );
-	}while(1);
+      memset( T->conform, 0, sizeof(INT) * nRow );
+   }while(1);
 
-	// Iteration is faster than recursion
-	// minus(T); 
+   // Iteration is faster than recursion
+   // minus(T); 
 }
 
 /* main main*/
 int main(int argc, char* argv[])
 {
-	TRSACT T;
-	TIMER_TYPE start_time = 0;
-	start_time = get_time();
-	
+   TRSACT T;
+   TIMER_TYPE start_time = 0;
+   start_time = get_time();
+   
 #ifndef HARDCODED_DATA
-	if( argc < 3 )
-	{
-		printf("Specify input and output files\n");
-		return 1;
-	}
-	const char * inFileName = argv[1];
-	const char * outFileName = argv[2];
+   if( argc < 3 )
+   {
+      printf("Specify input and output files\n");
+      return 1;
+   }
+   const char * inFileName = argv[1];
+   const char * outFileName = argv[2];
 #else
-	const char * inFileName = "weather.txt";
-	const char * outFileName = "weather.out";
+   const char * inFileName = "weather.txt";
+   const char * outFileName = "weather.out";
 #endif
 
-	TRSACT_file_load(&T, inFileName);
+   TRSACT_file_load(&T, inFileName);
 
-	TRSACT_init(&T);
+   TRSACT_init(&T);
 
-	minus(&T);
+   minus(&T);
 
    // Because didn't want to program a separate minus function for doing the horizontal removal..
    // ..we have this switch that will fake the data a bit
-	TRSACT_switch(&T);
+   TRSACT_switch(&T);
 
-	minus(&T);
+   minus(&T);
 
-	TRSACT_output_result(&T, outFileName);
+   TRSACT_output_result(&T, outFileName);
 
-	TRSACT_free(&T);
+   TRSACT_free(&T);
 
-	TIMER_TYPE total_time = get_time() - start_time;
-	TIMER_TYPE total_seconds = 0;
+   TIMER_TYPE total_time = get_time() - start_time;
+   TIMER_TYPE total_seconds = 0;
    // For getting the time in human-readable form (seconds)
 #ifdef _MSC_VER
-	LARGE_INTEGER timerFreq;
-	QueryPerformanceFrequency(&timerFreq);
-	total_seconds = (double)total_time/(double)timerFreq.QuadPart;
+   LARGE_INTEGER timerFreq;
+   QueryPerformanceFrequency(&timerFreq);
+   total_seconds = (double)total_time/(double)timerFreq.QuadPart;
 #else
-	total_seconds = total_time/1000.0;
+   total_seconds = total_time/1000.0;
 #endif
-	printf("Finished in about %4.2f seconds \n", total_seconds);
+   printf("Finished in about %4.2f seconds \n", total_seconds);
 #ifdef DEBUG_TIMER
-	TimerContainer::dump("minus_timer.txt");
+   TimerContainer::dump("minus_timer.txt");
 #endif
-	return 0;
+   return 0;
 }
 
 
